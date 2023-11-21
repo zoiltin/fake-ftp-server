20d33c5e8481cf3b25cf793c9d846d6618caf864
zoiltin <zoiltin0x@gmail.com>
2023-11-21 20:10:40 +0800

    support epsv method

--- fake-ftp-server.py
+++ fake-ftp-server.py
@@ -1,23 +1,31 @@
 from socket import *
 from multiprocessing import Process
+import time
 
 IP = "192.168.72.132"
 PORT = 1234
 ADDR_MAIN = (IP, PORT)
-ADDR_EXT = (IP,62501)
+ADDR_EXT = (IP,63568)
 
 def epsv(s_ext):
     s_ext.bind(ADDR_EXT)
     s_ext.listen()
-    connect, addr = s.accept()
-    connect.send(b'hello')
+    time.sleep(0.5)   # wait for 2th channel
+    s_ext.close()
+    return 0
+
+def clear_null(res):
+    new = []
+    for i in res:
+        if i != '':
+            new.append(i)
+    return new
 
 def center(connect):
     res = []
     connect.send(b"220 (vsFTPd 3.0.3)\n")
     while True:
         msg = connect.recv(1024)
-        print(msg.decode())
         data = ''
         rep = b'\n'
         if 'USER ' == msg.decode()[:5]:
@@ -35,25 +43,16 @@ def center(connect):
             s_ext = socket()
             p_ext = Process(target=epsv, args=(s_ext,))
             p_ext.start()
-            rep = b'229 Entering Extended Passive Mode (|||62501|)\n'
-            # connect.send(rep)
-            # data = connect.recv(1024).decode()
-            # p_ext.close()
-            # print(data)
-            # return 0
+            rep = b'229 Entering Extended Passive Mode (|||63568|)\n'
+            time.sleep(0.5)  # wait for 2th channel
         elif 'RETR ' == msg.decode()[:5]:
             data = msg.decode()[5:]
-        res.append(data)
-        # if rep == b'':
-        #     print('******  '+msg.decode())
-        # print('> '+(rep.decode()).replace('\n',''))
+        res.append(data[:-2])
         connect.send(rep)
         if 'RETR ' == msg.decode()[:5]:
-            # s.close()
-            print('asdadsasdadsasdasdasd')
-            s_ext.close()
-            p_ext.join()
-            return '.'.join(res)
+            time.sleep(1)  # wait for 2th channel
+            p_ext.close()
+            return '/'.join(clear_null(res))
 
 if __name__ == '__main__':
     s = socket()
@@ -63,4 +62,5 @@ if __name__ == '__main__':
     data = center(connect)
     with open('res.txt','w+') as f:
         f.write(data)
+    s.close()
     
